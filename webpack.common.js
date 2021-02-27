14b07226f297ab95818560aadc807111826cd24b
teunlao <teunlao@gmail.com>
2021-02-27 16:53:40 +0300

    chore: add multiple configs for webpack

---
+++ webpack.common.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const WebpackBar = require('webpackbar');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.tsx',
+  output: {
+    path: path.resolve('dist'),
+    filename: '[name].[hash:6].bundle.js',
+    publicPath: process.env.BASE_URL,
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.(ts|js)x?$/],
+        use: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      favicon: './public/favicon.png',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash:6].css',
+    }),
+    new WebpackBar(),
+  ],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
+  devtool: 'source-map',
+};
---
+++ webpack.dev.js
@@ -0,0 +1,23 @@
+const CopyPlugin = require('copy-webpack-plugin');
+
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common')
+
+module.exports = merge(common, {
+  mode: 'development',
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: 'api/test.php', to: 'test.php' }],
+    }),
+  ],
+  devServer: {
+    hot: true,
+    stats: 'minimal',
+    historyApiFallback: true,
+    writeToDisk: true,
+  },
+  watchOptions: {
+    aggregateTimeout: 300,
+    poll: 1000,
+  },
+})
